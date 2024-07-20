87612df7c19f991b901449a2b16edfb574c3c7bf
Nourhan Nabil <nourhan.nabill@outlook.com>
2024-07-20 19:34:41 +0200

    calling the inference code in controller

--- Controllers/ProjectController.py
+++ Controllers/ProjectController.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status, File, UploadFile
 from fastapi.responses import JSONResponse
 
 from Utils.FileUtils import FileUtils
+from inference import main as inference_main
 
 lip_syncing_router = APIRouter()
 
@@ -15,6 +16,7 @@ async def generate_lip_syncing(video: UploadFile = File(...), audio: UploadFile 
     audio_path = FileUtils.save_audio_video(audio.filename,audio_content)
 
     try:
+        inference_main(video_path,audio_path)
         return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Lip-syncing video generated successfully"})
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
